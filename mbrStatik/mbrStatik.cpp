1de2d0082c87548ae68a9e8760d5f0f376cf8105
fahreziahmad <alfahreziahmadsyahyana@gmail.com>
2024-05-14 18:07:01 +0700

    add variabel dan function

--- mbrStatik/mbrStatik.cpp
+++ mbrStatik/mbrStatik.cpp
@@ -19,3 +19,15 @@ public:
 		setID();
 	}
 };
+int mahasiswa::nim = 113;
+void mahasiswa::setID()
+{
+	id = nim++;
+}
+
+void mahasiswa::printAll()
+{
+	cout << "ID   = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
