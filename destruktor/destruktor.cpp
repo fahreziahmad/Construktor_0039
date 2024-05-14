b9febecdaef342de8e4402bc55f351562c1bdf54
fahreziahmad <alfahreziahmadsyahyana@gmail.com>
2024-05-14 19:25:10 +0700

    add variabel

--- destruktor/destruktor.cpp
+++ destruktor/destruktor.cpp
@@ -12,3 +12,16 @@ public:
 	void cetakData();
 	void isiData();
 };
+// Definisi member Function
+angka::angka(int i) { // Construktor
+	panjang = i;
+	arr = new int(i);
+	isiData();
+}
+
+angka::~angka() { // Destruktor
+	cout << endl;
+	cetakData();
+	delete[]arr;
+	cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
