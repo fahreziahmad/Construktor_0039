d5aa0bcb35dd4f3bf1a6e8b938924e9ae4293cfc
fahreziahmad <alfahreziahmadsyahyana@gmail.com>
2024-05-14 19:54:36 +0700

    add public member mhsiswa

--- construktor1/construktor1.cpp
+++ construktor1/construktor1.cpp
@@ -7,3 +7,22 @@ class Mahasiswa
 private:
     int nim;
     string nama;
+public:
+    Mahasiswa()
+    {
+        nim = 0;
+        nama = "";
+    };
+    Mahasiswa(int iNim)
+    {
+        nim = iNim;
+    };
+    Mahasiswa(string iNama)
+    {
+        nama = iNama;
+    };
+    Mahasiswa(int iNim, string iNama)
+    {
+        nim = iNim;
+        nama = iNama;
+    };
